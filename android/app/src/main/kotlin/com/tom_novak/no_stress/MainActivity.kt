64917e67fc889541a3ccb7700351717d38235d08
Tomáš Novák <hello@tom-novak.com>
2020-03-31 16:25:29 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/tom_novak/no_stress/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tom_novak.no_stress
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
